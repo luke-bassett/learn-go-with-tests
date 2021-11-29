710c759f0f88bb8576e67652d74a0d96004df689
Luke Bassett <luke@Lukes-MacBook-Pro.local>
2021-11-28 21:42:11 -0800

    refactored

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,15 @@
 package iteration
 
-func Repeat(character string) string {
+import "strings"
+
+func Repeat(character string, n int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < n; i++ {
 		repeated += character
 	}
 	return repeated
+}
+
+func Replace(s, old, new string) string {
+	return strings.Replace(s, old, new, -1)
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,18 +1,37 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 3)
+	expected := "aaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
 
+func TestReplace(t *testing.T) {
+	replaced := Replace("hip", "i", "o")
+	expected := "hop"
+
+	if replaced != expected {
+		t.Errorf("expected %q but got %q", expected, replaced)
+	}
+
+}
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("ha", 3)
+	fmt.Println(repeated)
+	// Output: hahaha
 }
