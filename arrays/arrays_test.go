79b587e9e3039d04447f84aabe4ef7967ef9f2a5
Luke Bassett <luke@Lukes-MacBook-Pro.local>
2021-11-28 22:30:02 -0800

    added SumAllTails

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -26,3 +26,22 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("two slices of len 2", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{2, 4, 7})
+		want := []int{0, 11}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
